68ad4c38d94a8bbeca141fde984e30217820d82a
Fernet32 <38507657+Fernet32@users.noreply.github.com>
2019-10-13 13:56:51 +0200

    OpenLab-01.01

---
+++ OpenLab-01.01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OPEN_LAB_Uloha_01._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string zadane = Console.ReadLine();
+            int zadanečislo = int.Parse(zadane);
+
+            if (zadanečislo < 10)
+            {
+                Console.WriteLine("This number is less then 10");
+            }
+            else
+            {
+                Console.WriteLine("This number is greater or equal to 10");
+            }
+        }
+    }
+}
