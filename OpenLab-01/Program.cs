e5e133df4b48fa63ff47b7c29a4081a9f7f637d5
Fernet32 <38507657+Fernet32@users.noreply.github.com>
2019-10-13 13:50:18 +0200

    OpenLab-01

---
+++ OpenLab-01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenLab_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Xamarin Lab!");
+        }
+    }
+}
