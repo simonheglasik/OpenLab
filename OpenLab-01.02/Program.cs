635b47bde96c10e574ba9ceadde65a1fb64a0076
Fernet32 <38507657+Fernet32@users.noreply.github.com>
2019-10-13 13:58:51 +0200

    OpenLab-01.02

---
+++ OpenLab-01.02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OPEN_LAB_Uloha_01._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string a = Console.ReadLine();
+                string b = Console.ReadLine();
+                int A = int.Parse(a);
+                int B = int.Parse(b);
+                int result = Sum(A, B);
+
+                Console.WriteLine(result);
+            }
+        }
+
+        public static int Sum(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+    }
+}
