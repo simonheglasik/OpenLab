59c78df4228f723efc3b48b40a5034560d377b00
Simon Heglasik <38507657+simonheglasik@users.noreply.github.com>
2019-10-14 17:21:39 +0200

    Add files via upload

---
+++ OpenLab-Uloha-01.04/OpenLab-Uloha-01.04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OPEN_LAB_Uloha_01._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            while (true)
+            {
+
+                string basse = Console.ReadLine();
+                string height = Console.ReadLine();
+                int B = int.Parse(basse);
+                int H = int.Parse(height);
+                int result = Sum(B, H);
+                Console.WriteLine(result);
+            }
+
+        }
+        public static int Sum(int z, int v)
+        {
+            int sum = (z * v) / 2;
+            return sum;
+        }
+
+
+    }
+}
