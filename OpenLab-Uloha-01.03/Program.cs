8d5bc544aa67fd42aa9d7f3383e47ecbd72a561c
simonheglasik <simonheglasik@gmail.com>
2019-10-14 16:58:45 +0200

    OpenLab 01.03

---
+++ OpenLab-Uloha-01.03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace OPEN_LAB_Uloha_01._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            while (true)
+            {
+                string a = Console.ReadLine();
+
+                int A = int.Parse(a);
+
+                int result = Sum(A);
+
+                Console.WriteLine(result);
+            }
+        }
+        public static int Sum(int a)
+        {
+            return a + 1;
+        }
+    }
+
+}
