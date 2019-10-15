aab7e47534e74921f9b8d20f25f0387d62f37f5a
Simon Heglasik <38507657+simonheglasik@users.noreply.github.com>
2019-10-15 18:11:55 +0200

    Add files via upload

---
+++ OpenLab-Uloha-01.04/OpenLab-Uloha-02-01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OpenLab_Uloha_02_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            double [] araj = { 2, 3, 1, 4, 6 };
+            Console.WriteLine(araj[1]);
+            
+        }
+    }
+}
